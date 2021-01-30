d5f922c5c8ac90b76c8a5a654dbae955912ec240
Ignacio Garcia <69227346+Agarc4777@users.noreply.github.com>
2021-01-30 14:12:25 -0600

    added nodemailer file which allows us to send emails

---
+++ nodemailer/nodemailer.js
@@ -0,0 +1,23 @@
+var nodemailer = require('nodemailer');
+
+var transport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: "thefourmeatheads@gmail.com",
+        pass: "The4m34theads"
+      }
+  });
+
+var message = {
+  from: '',
+  to: '',
+  subject: '',
+  text: ''
+};
+transport.sendMail(message, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
