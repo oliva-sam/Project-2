ca443bcc6636bcd52907bc04484bd8d706e92fe1
Xavier Guzman <guzmanxavi112@gmail.com>
2021-02-02 20:46:03 -0600

    added api controller when user logs in and creates user data

--- controller/html_controller.js
+++ controller/html_controller.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
 
-const user = require("../models/user.js");
+// const user = require("../models/user.js");
 
-router.get("/", function (req, res) {
-  if (req.user) {
-    res.redirect("/Users");
-  }
-});
+// router.get("/", function (req, res) {
+//   if (req.user) {
+//     res.redirect("/Users");
+//   }
+// });
