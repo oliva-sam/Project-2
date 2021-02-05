c62be71e0f4a03ac66fa6e985884f857c77f446b
Ignacio Garcia <69227346+Agarc4777@users.noreply.github.com>
2021-02-04 20:26:40 -0600

    random stuff

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ///// DEPENDENTS /////
 
 const express = require("express");
@@ -20,27 +21,41 @@ const passport = require("./config/passport");
 //// MIDDLEWARE////
 
 // parse posts requests & passport authentication
+=======
+// Requiring necessary npm packages
+var express = require("express");
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
+
+// Setting up port and requiring models for syncing
+var PORT = process.env.PORT || 8080;
+var db = require("./models");
+
+// Creating express app and configuring middleware needed for authentication
+var app = express();
+>>>>>>> main
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// access to all files in public folder
 app.use(express.static("public"));
-
-//express-session middleware
-app(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-
-// passport middleware
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
+<<<<<<< HEAD
 //// Require Routes section IN PROGRESS ////
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 
+=======
+>>>>>>> main
 //// SYNC THE DATABASE THEN LISTEN TO PORT ////
 db.sequelize.sync().then(function () {
-    app.listen(PORT, function () {
-        return console.log(`Listening on http://localhost:${PORT}`)
-    })
+  app.listen(PORT, function () {
+    return console.log(`Listening on http://localhost:${PORT}`);
+  });
 });
