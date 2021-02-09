dbb0c10aa6500e7c6fa790eed7f1531ef78824c9
Melody Monarch <oliva.sam.so@gmail.com>
2021-02-08 20:26:59 -0600

    users are able to add goals to their page, update if they're complete, or delete their goals

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -1,4 +1,63 @@
 console.log("hi");
 
-// clients page functionality
+$(document).ready(function () {
 
+////////// ADD GOAL ////////////    
+    $("#goalBtn").on("click", function (event) {
+        event.preventDefault();
+
+        let newGoal = {
+            goals: $("#userGoal").val().trim(),
+            complete: 0
+        };
+
+        $.post("/api/client", {
+            goals: newGoal.goals,
+            complete: newGoal.completed
+        })
+            .then(function () {
+                console.log("New Goal Added...");
+                location.reload();
+            })
+            .catch(function (err) {
+                console.log(err);
+                alert("There was an error")
+            })
+    });
+
+///////////// COMPLETE GOAL /////////////    
+    $(".completeBtn").on("click", function (event) {
+        let id = $(this).data("id");
+
+        $.put("api/client/" + id, {
+            complete: 1
+        })
+            .then(function () {
+                console.log(`You have completed ${id}...`);
+                location.reload();
+            })
+            .catch(function (err) {
+                console.log(err);
+                alert("There was an error")
+            })
+    });
+
+///////////// DELETE GOAL //////////////
+    $(".deleteBtn").on("click", function (event) {
+        let id = $(this).data("id");
+
+        $.delete("api/client/" + id, {
+        }).then(
+            function () {
+                console.log(`You have deleted ${id}...`);
+                location.reload();
+            }
+        )
+        .catch(function (err) {
+            console.log(err);
+            alert("There was an error")
+        })
+
+    });
+
+});
