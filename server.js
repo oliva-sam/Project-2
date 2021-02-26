ae7dfe96e4154cc2254713b1a4a009113cbab6e0
Melody Monarch <oliva.sam.so@gmail.com>
2021-02-25 20:06:22 -0600

    setting everything up for heroku

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-const exphbs = require("express-handlebars");
+var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 // We need to use sessions to keep track of our user's login status
